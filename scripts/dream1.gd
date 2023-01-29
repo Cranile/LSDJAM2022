0c82288b18249e7ffc9534305011ea64890d73ec
NicolasCrapanzano <nicolaslcrapanzano@gmail.com>
2023-01-29 16:35:02 -0300

    stuff

--- scripts/dream1.gd
+++ scripts/dream1.gd
@@ -1,13 +1,16 @@
 extends Spatial
 
 var dreamID = 1
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+onready var figureSpawnPoint = get_node("ViewportContainer/Viewport/map/figureSpawnPoint")
+export var figureList = ["","",""]
+var figure
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawnFigure()
 	pass # Replace with function body.
 
 func getDreamID():
@@ -15,3 +18,15 @@ func getDreamID():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func spawnFigure():
+	var figureID = get_node("/root/main").figurine
+	figure = load(figureList[figureID - 1]).instance()
+	figureSpawnPoint.add_child(figure)
+	figure.rotate_y(3.141593) ## its on radians!!!!
+	$ViewportContainer/Viewport/map/figureSpawnPoint/waveTimer.start()
+
+
+func _on_Timer_timeout():
+	figure.playAnimation("wave")
+	pass # Replace with function body.
